e4a096b05d6ac370bc34d0e1535d49dc1e4c2d04
cwees <chrishe44@gmail.com>
2024-01-31 17:41:18 -0600

    initial commit for add file and missing methods

---
+++ For.java
@@ -0,0 +1,22 @@
+/**
+ * Derived class that represents a for statment in SILLY
+ * 
+ * @author Chris He
+ * @version 1/30/2024
+ * 
+ */
+public class For extends Statement {
+    private Integer count;
+    private Integer finalcount;
+
+    public void execute() throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+
+    public String toString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
+
+}
