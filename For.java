0198b38fafd3a51c78a008a9a2ee3cf646e63d2a
chrisdesktopreplacement <chrishe44@gmail.com>
2024-02-03 21:55:36 -0600

    stuff

--- For.java
+++ For.java
@@ -6,23 +6,47 @@
  * 
  */
 public class For extends Statement {
-    private Integer count;
+    private Integer initial;
     private Integer finalcount;
+    private String iterator;
+    private Compound body;
 
+    // todo test
     public For(TokenStream input) throws Exception {
         if (!input.next().toString().equals("for")) {
             throw new Exception("SYNTAX ERROR: Malformed if statement");
         }
+        if (!(input.next().getType() == Token.Type.STRING_LITERAL)) {
+            throw new Exception("SYNTAX ERROR: MALFORMED if,");
+        }
+        this.iterator = input.toString();
+        if (!input.next().toString().equals("from")) {
+            throw new Exception("SYNTAX ERROR: Malformed if statement");
+        }
+        if (!(input.next().getType() == Token.Type.INTEGER_LITERAL)) {
+            throw new Exception("SYNTAX ERROR: Malformed if statement");
+        } else {
+            this.initial = Integer.valueOf(input.toString());
+        }
+        if (!input.next().toString().equals("to")) {
+            throw new Exception("SYNTAX ERROR: Malformed if statement");
+        }
+        if (!(input.next().getType() == Token.Type.INTEGER_LITERAL)) {
+            throw new Exception("SYNTAX ERROR: Malformed if statement");
+        } else {
+            this.finalcount = Integer.valueOf(input.toString());
+        }
+        this.body = new Compound(input);
     }
 
     public void execute() throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+        for (int i = this.initial; i < this.finalcount; i++) {
+            this.body.execute();
+        }
     }
 
     public String toString() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        return "for " + this.iterator + " from " + this.initial + " to " + this.finalcount + this.body;
     }
 
 }
--- Print.java
+++ Print.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Derived class that represents an output statement in the SILLY language.
  * 
@@ -5,7 +7,15 @@
  * @version 12/22/23
  */
 public class Print extends Statement {
-    private Expression expr;
+    // private Expression expr;
+    private ArrayList<Expression> exprs;
+
+    // Modify the Print class so that it can print multiple expressions, separated
+    // by commas. When printed, the values should all appear on the same line,
+    // separated by a single space. This will require adding field(s) and modifying
+    // the constructor (to be able to read and store an arbitrary number of
+    // expressions), the execute method (to display all the expression values on a
+    // single line), and the toString method (to display the general format).
 
     /**
      * Reads in a print statement from the specified TokenStream.
@@ -13,12 +23,17 @@ public class Print extends Statement {
      * @param input the stream to be read from
      */
     public Print(TokenStream input) throws Exception {
+        this.exprs = new ArrayList<Expression>();
+
         if (!input.next().toString().equals("print")) {
             throw new Exception("SYNTAX ERROR: Malformed print statement");
         }
         if (!input.next().toString().equals("(")) {
             throw new Exception("SYNTAX ERROR: Missing '(' in print statement");
         }
+        
+        this.exprs.add(new Expression(input));
+
         this.expr = new Expression(input);
         if (!input.next().toString().equals(")")) {
             throw new Exception("SYNTAX ERROR: Missing ')' in print statement");
@@ -29,12 +44,23 @@ public class Print extends Statement {
      * Executes the current output statement.
      */
     public void execute() throws Exception {
-        String str = this.expr.evaluate().toString();
-        if (str.charAt(0) == '"') {
-            System.out.println(str.substring(1, str.length() - 1));
-        } else {
-            System.out.println(str);
+        String tester = "";
+        for (int i = 0; i < this.exprs.size(); i++) {
+            String str = this.exprs.get(i).evaluate().toString();
+            if (str.charAt(0) == '"') {
+                tester += str.substring(1, str.length() - 1) + ", ";
+            } else {
+                tester += str + ", ";
+            }
         }
+        System.err.println(tester);
+
+        // String str = this.expr.evaluate().toString();
+        // if (str.charAt(0) == '"') {
+        // System.out.println(str.substring(1, str.length() - 1));
+        // } else {
+        // System.out.println(str);
+        // }
     }
 
     /**
@@ -43,6 +69,13 @@ public class Print extends Statement {
      * @return the String representation of this statement
      */
     public String toString() {
-        return "print(" + this.expr + ")";
+        // return "print(" + this.expr + ")";
+
+        String str = "print( ";
+        for (int i = 0; i < this.exprs.size(); i++) {
+            str = str + this.exprs.get(i) + " ";
+        }
+        str = str + ")";
+        return str;
     }
 }
