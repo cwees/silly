6996812e0210e97ab3279e769d76749c4c43660b
cwees <chrishe44@gmail.com>
2024-02-22 13:45:27 -0600

    morestuffdown

---
+++ SubCall.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class SubCall extends Statement {
+    private Token functionName;
+    private ArrayList<Expression> parameters;
+
+    public SubCall(TokenStream input) throws Exception {
+        if (!input.next().toString().equals("call")) {
+            throw new Exception("SYNTAX ERROR: Malformed subcall statement");
+        }
+        this.functionName = input.next();
+        if (this.functionName.getType() != Token.Type.IDENTIFIER) {
+            throw new Exception("SYNTAX ERROR: Malformed identifier in subcall statement");
+        }
+        if (!input.next().toString().equals("(")) {
+            throw new Exception("SYNTAX ERROR: Malformed subcall statement");
+        }
+        this.parameters = new ArrayList<Expression>();
+        while (!input.lookAhead().toString().equals(")")) {
+            input.next();
+            this.parameters.add(new Expression(input));
+            if (input.lookAhead().toString().equals(",")) {
+                continue;
+            }
+        }
+        if (!input.next().toString().equals(")")) {
+            throw new Exception("SYNTAX ERROR: Malformed subcall statement");
+        }
+        // <subcall> --> 'call' <id> '(' [ <expr> { ',' <expr> } ] ')'
+    }
+
+    @Override
+    public void execute() throws Exception {
+
+        // check if subdec and subcall parameter counts line up
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+
+    @Override
+    public String toString() {
+        String str = "call " + this.functionName + " (";
+        for (int i = 0; i < this.parameters.size(); i++) {
+            str += this.parameters.get(i).toString() + ", ";
+        }
+        str = str.substring(0, str.length() - 2);
+        str += ")";
+        return str;
+    }
+
+}
--- SubDec.java
+++ SubDec.java
@@ -1,45 +1,63 @@
 import java.util.ArrayList;
 
 public class SubDec extends Statement {
-    private Token identifier;
-    private ArrayList<Token> identifiers;
+    private Token functionName;
+    private ArrayList<Token> parameters;
     private Compound body;
 
-    @Override
-    public While(TokenStream input) throws Exception:
-        this.identifiers = new ArrayList<Token>();
-
+    public SubDec(TokenStream input) throws Exception {
         if (!input.next().toString().equals("sub")) {
             throw new Exception("SYNTAX ERROR: Malformed subroutine statement");
         }
-        this.identifier = input.next();
-        if (this.identifier.getType() != Token.Type.IDENTIFIER) {
-            throw new Exception("SYNTAX ERROR: Malformed subroutine statement");
+        this.functionName = input.next();
+        if (this.functionName.getType() != Token.Type.IDENTIFIER) {
+            throw new Exception("SYNTAX ERROR: Malformed identifier in subroutine statement");
         }
         if (!input.next().toString().equals("(")) {
             throw new Exception("SYNTAX ERROR: Malformed subroutine statement");
         }
-        if(!input.next().toString().equals(")")){
-        //if there are multiple parameters
-        //add to arraylist
-        //check for syntax ","
-        }
-        this.body = new Compound(input);
+        this.parameters = new ArrayList<Token>();
+        while (input.lookAhead().getType() == Token.Type.IDENTIFIER) {
+            this.parameters.add(input.next());
+            if (!input.lookAhead().toString().equals(",")) {
+                input.next();
+                continue;
+            } else if (!input.lookAhead().toString().equals(")")) {
+                break;
+            } else {
+                System.out.println(input.toString());
+                throw new Exception("SYNTAX ERROR: Malformed subroutine statement");
+            }
 
+        }
+        if (!input.next().toString().equals(")")) {
+            throw new Exception("SYNTAX ERROR: Malformed subroutine statement");
+        }
+
+        this.body = new Compound(input);
+        // ()
+        // (item)
+        // (item, item)
 
     }
     // <subdecl> --> 'sub' <id> '(' [ <id> { ',' <id> } ] ')' <compound>
 
-    // <subcall> --> 'call' <id> '(' [ <expr> { ',' <expr> } ] ')'
-    @Override
     public String toString() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        String str = "sub " + this.functionName + "( ";
+        for (int i = 0; i < this.parameters.size(); i++) {
+            str += this.parameters.get(i).toString() + ", ";
+        }
+        str = str.substring(0, str.length() - 2);
+        str += ") " + this.body.toString();
+        return str;
     }
 
-    @Override
     public void execute() throws Exception {
         // TODO Auto-generated method stub
+
+        // iterate through this.parameters, error if they have been declared before
+        // hand, else initialize
+        // store body somehow
         throw new UnsupportedOperationException("Unimplemented method 'execute'");
     }
 
